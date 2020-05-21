9abb5f38a602b820af5332e0c351bf48a2bae315
Andy <wlo@fortinet.com>
2020-05-20 18:48:34 -0700

    Updates

---
+++ precache-manifest.c743948bf4baeb9f1614c8e8c987d952.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8593a54fe286b36124b9589fa96d79c9",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5a494d46a3a660b1a03d",
+    "url": "/monsters-rolodex/static/css/main.1d42e973.chunk.css"
+  },
+  {
+    "revision": "d001ce9c3e21c3f54086",
+    "url": "/monsters-rolodex/static/js/2.fae6f3df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.fae6f3df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a494d46a3a660b1a03d",
+    "url": "/monsters-rolodex/static/js/main.4f145853.chunk.js"
+  },
+  {
+    "revision": "0f6210bbb6aee2db2a20",
+    "url": "/monsters-rolodex/static/js/runtime-main.8508266c.js"
+  }
+]);
